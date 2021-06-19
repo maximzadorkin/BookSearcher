c312953eeefcb645a80c05f327832d1601a3003d
Maxim Zadorkin <57238571+maximzadorkin@users.noreply.github.com>
2021-06-19 17:00:16 +0300

    Searcher:bugfix change animation

--- src/components/Searcher/Searcher.tsx
+++ src/components/Searcher/Searcher.tsx
@@ -2,13 +2,16 @@ import * as React from 'react'
 import { useState, useEffect } from 'react'
 import classes from './Searcher.module.sass'
 import IconUrl from '../../ui/icon-searcher.svg'
-import Button from '../Button/Button'
 
 interface ISearcher {
     search: (value: string) => void
     placeholder?: string
     autoSearch: boolean
     autoSearchDelay?: number /* in miliseconds */
+}
+interface IStyles {
+    btn: string
+    input: string
 }
 
 const Searcher = ({
@@ -17,10 +20,15 @@ const Searcher = ({
     autoSearch,
     autoSearchDelay,
 }: ISearcher): React.ReactElement => {
-    const initInputClasses: string[] = [classes.input]
-    const initBtnClasses: string[] = [classes.button]
-    const [InputClasses, setInputClasses] = useState(initInputClasses)
-    const [BtnClasses, setBtnClasses] = useState(initBtnClasses)
+    const initStyles: IStyles = {
+        btn: classes.button,
+        input: classes.input,
+    }
+    const initActiveStyles: IStyles = {
+        btn: [classes.button, classes.active].join(' '),
+        input: [classes.input, classes.active].join(' '),
+    }
+    const [styles, setStyles] = useState(initStyles)
     const [value, setValue] = useState('')
     const [dateOfChanges, setDateOfChanges] = useState(null)
     const [searched, setSearched] = useState(false)
@@ -59,29 +67,17 @@ const Searcher = ({
         <form className={classes.wrapper} onSubmit={submitHandler}>
             <input
                 type='text'
-                className={InputClasses.join(' ')}
+                className={styles.input}
                 placeholder={placeholder || ''}
-                onFocus={() => {
-                    setInputClasses([classes.input, classes.active])
-                    setBtnClasses([classes.button, classes.active])
-                }}
-                onBlur={() => {
-                    setInputClasses(initInputClasses)
-                    setBtnClasses(initBtnClasses)
-                }}
+                onFocus={() => setStyles(initActiveStyles)}
+                onBlur={() => setStyles(initStyles)}
                 onChange={onChangeHandler}
             />
             <button
                 type='submit'
-                className={BtnClasses.join(' ')}
-                onFocus={() => {
-                    setInputClasses([classes.input, classes.active])
-                    setBtnClasses([classes.button, classes.active])
-                }}
-                onBlur={() => {
-                    setInputClasses(initInputClasses)
-                    setBtnClasses(initBtnClasses)
-                }}
+                className={styles.btn}
+                onFocus={() => setStyles(initActiveStyles)}
+                onBlur={() => setStyles(initStyles)}
             >
                 <img src={IconUrl} alt='Поиск' className={classes.icon} />
             </button>
